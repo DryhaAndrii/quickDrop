6d986b98f2a9111c992ec367cc139ae0da2243f3
Dryha Andriy <andrii.dr@fernir.co>
2025-02-18 14:33:20 +0200

    first commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Quick Drop | Fast File Sharing",
+  description:
+    "Quick Drop allows you to share files instantly in temporary rooms. Upload, share, and download files before they disappear!",
+  keywords: [
+    "file sharing",
+    "temporary file storage",
+    "secure file transfer",
+    "instant file sharing",
+    "file exchange",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
